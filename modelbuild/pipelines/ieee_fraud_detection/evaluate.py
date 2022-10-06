5dec7881929847eb1116d6e4685aefcf760ceb6c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-06 05:43:13 +0000

    Adding tarfile member sanitization to extractall()

--- modelbuild/pipelines/ieee_fraud_detection/evaluate.py
+++ modelbuild/pipelines/ieee_fraud_detection/evaluate.py
@@ -55,7 +55,29 @@ if __name__ == "__main__":
 
     model_path = f"{base_dir}/models"
     with tarfile.open(model_path + "/model.tar.gz") as tar:
-        tar.extractall(path=model_path)
+        
+        import os
+        
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path=model_path)
 
     # The process of saving and loading models has changed since XGBoost version 1.3.x.
     estimator = xgb.Booster()
